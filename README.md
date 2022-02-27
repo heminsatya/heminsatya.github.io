b3222e38082c2e4ba3739586191282af1f169d9a
heminsatya <heminsatya@outlook.com>
2022-02-27 18:33:45 +0330

    Updating my GitHub page

--- README.md
+++ README.md
@@ -1,37 +1,14 @@
-## Welcome to GitHub Pages
+## Welcome to My GitHub Page
 
-You can use the [editor on GitHub](https://github.com/heminsatya/heminsatya.github.io/edit/main/README.md) to maintain and preview the content for your website in Markdown files.
+Hello World!
 
-Whenever you commit to this repository, GitHub Pages will run [Jekyll](https://jekyllrb.com/) to rebuild the pages in your site, from the content in your Markdown files.
+I'm Hemin Satya, a freelance programmer. Someone who love writing and sharing code.
 
-### Markdown
+I have been a computer programmer since 2008, and I have written code for desktop, web, and mobile.
+However, I worked mostly as a Web Developer.
 
-Markdown is a lightweight and easy-to-use syntax for styling your writing. It includes conventions for
+Please let me know your precious comments, observations, and suggestions.
+([GitHub](https://github.com/heminsatya))
+([Twitter](https://twitter.com/heminsatya))
 
-```markdown
-Syntax highlighted code block
-
-# Header 1
-## Header 2
-### Header 3
-
-- Bulleted
-- List
-
-1. Numbered
-2. List
-
-**Bold** and _Italic_ and `Code` text
-
-[Link](url) and ![Image](src)
-```
-
-For more details see [GitHub Flavored Markdown](https://guides.github.com/features/mastering-markdown/).
-
-### Jekyll Themes
-
-Your Pages site will use the layout and styles from the Jekyll theme you have selected in your [repository settings](https://github.com/heminsatya/heminsatya.github.io/settings/pages). The name of this theme is saved in the Jekyll `_config.yml` configuration file.
-
-### Support or Contact
-
-Having trouble with Pages? Check out our [documentation](https://docs.github.com/categories/github-pages-basics/) or [contact support](https://support.github.com/contact) and we’ll help you sort it out.
+Thanks for visiting my webpage.
---
+++ statics/js/main.js
@@ -0,0 +1,180 @@
+// Redirect function
+function redirect(url) {
+    window.location.href = url;
+}
+
+
+// Validate email
+function validateEmail(email) {
+    const result = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return result.test(String(email).toLowerCase());
+}
+
+
+// Contact form
+document.querySelector("#contact-form button").addEventListener("click", function() {
+
+    // Result
+    let result = true;
+
+    // Change button text temporarily
+    this.innerHTML = "Submit..."
+
+    // Form elements
+    let name = document.querySelector("#contact-form [name=name]");
+    let email = document.querySelector("#contact-form [name=email]");
+    let subject = document.querySelector("#contact-form [name=subject]");
+    let message = document.querySelector("#contact-form [name=message]");
+
+    // Check the name
+    if (name.value == null || name.value == "") {
+        name.classList.add("is-invalid");
+        name.parentElement.querySelector(".text-info").innerHTML = "Name is required";
+        result = false;
+
+    } else {
+        name.classList.remove("is-invalid");
+        name.parentElement.querySelector(".text-info").innerHTML = "";
+    }
+
+
+    // Check the email
+    if (email.value == null || email.value == "") {
+        email.classList.add("is-invalid");
+        email.parentElement.querySelector(".text-info").innerHTML = "Email is required";
+        result = false;
+
+    } else if (!validateEmail(email.value)) {
+        email.classList.add("is-invalid");
+        email.parentElement.querySelector(".text-info").innerHTML = "Email is invalid";
+        result = false;
+
+    } else {
+        email.classList.remove("is-invalid");
+        email.parentElement.querySelector(".text-info").innerHTML = "";
+    }
+
+
+    // Check the subject
+    if (subject.value == null || subject.value == "") {
+        subject.classList.add("is-invalid");
+        subject.parentElement.querySelector(".text-info").innerHTML = "Subject is required";
+        result = false;
+
+    } else {
+        subject.classList.remove("is-invalid");
+        subject.parentElement.querySelector(".text-info").innerHTML = "";
+    }
+
+
+    // Check the message
+    if (message.value == null || message.value == "") {
+        message.classList.add("is-invalid");
+        message.parentElement.querySelector(".text-info").innerHTML = "Message is required";
+        result = false;
+
+    } else if (message.value.length > 255) {
+        message.classList.add("is-invalid");
+        message.parentElement.querySelector(".text-info").innerHTML = "Message is too long";
+        result = false;
+
+    } else {
+        message.classList.remove("is-invalid");
+        message.parentElement.querySelector(".text-info").innerHTML = "";
+    }
+
+
+    // Change button text back
+    this.innerHTML = "Submit";
+
+
+    // Check results
+    if (result) {
+        // Show alert message
+        document.querySelector("#form-result").classList.add("alert");
+        document.querySelector("#form-result").classList.add("alert-success");
+        document.querySelector("#form-result").innerHTML = "Thanks for your message!";
+        document.querySelector("#form-result").style.opacity = "1";
+
+        // Prepare the email body
+        let body = `${name.value} <${email.value}>\n`;
+        body += `${message.value}`
+
+        // Submit form
+        window.open(`mailto:heminsatya@outlook.com?subject=${subject.value}&body=${encodeURIComponent(body)}`);
+
+        // Reset form
+        document.querySelector("#contact-form").reset();
+    }
+
+});
+
+
+// Contact form name
+document.querySelector("#contact-form [name=name]").addEventListener("blur", function() {
+    // Check the name
+    if (this.value == null || this.value == "") {
+        this.classList.add("is-invalid");
+        this.parentElement.querySelector(".text-info").innerHTML = "Name is required";
+        result = false;
+
+    } else {
+        this.classList.remove("is-invalid");
+        this.parentElement.querySelector(".text-info").innerHTML = "";
+    }
+});
+
+
+// Contact form email
+document.querySelector("#contact-form [name=email]").addEventListener("blur", function() {
+    // Check the email
+    if (this.value == null || this.value == "") {
+        this.classList.add("is-invalid");
+        this.parentElement.querySelector(".text-info").innerHTML = "Email is required";
+        result = false;
+
+    } else if (!validateEmail(this.value)) {
+        this.classList.add("is-invalid");
+        this.parentElement.querySelector(".text-info").innerHTML = "Email is invalid";
+        result = false;
+
+    } else {
+        this.classList.remove("is-invalid");
+        this.parentElement.querySelector(".text-info").innerHTML = "";
+    }
+});
+
+
+// Contact form subject
+document.querySelector("#contact-form [name=subject]").addEventListener("blur", function() {
+    // Check the subject
+    if (this.value == null || this.value == "") {
+        this.classList.add("is-invalid");
+        this.parentElement.querySelector(".text-info").innerHTML = "Subject is required";
+        result = false;
+
+    } else {
+        this.classList.remove("is-invalid");
+        this.parentElement.querySelector(".text-info").innerHTML = "";
+    }
+});
+
+
+// Contact form body
+document.querySelector("#contact-form [name=message]").addEventListener("blur", function() {
+    // Check the body
+    if (this.value == null || this.value == "") {
+        this.classList.add("is-invalid");
+        this.parentElement.querySelector(".text-info").innerHTML = "Message is required";
+        result = false;
+
+    } else if (this.value.length > 255) {
+        this.classList.add("is-invalid");
+        this.parentElement.querySelector(".text-info").innerHTML = "Message is too long";
+        result = false;
+
+    } else {
+        this.classList.remove("is-invalid");
+        this.parentElement.querySelector(".text-info").innerHTML = "";
+    }
+});
